5480cf3f936785c9160b405b83a4451b28d1210d
Shaun Gillon <shaun@oddpandadesign.co.uk>
2018-09-16 15:10:49 +0100

    Base Commit

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index.js',
+    libraryTarget: 'commonjs2' // NOTE Hey, our Output should be an exportable module!
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /(node_modules|bower_components|build)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      }
+    ]
+  },
+  externals: {
+    'react': 'commonjs react' // NOTE this line is just to use the React dependency of our parent-testing-project instead of using our own React.
+  }
+};
